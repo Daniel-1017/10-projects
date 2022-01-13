02a0dd272b0522737748f9386ac07a9652f8bff7
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-13 20:20:45 +0100

    added popular meals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import PopularMeals from "./components/PopularMeals/PopularMeals";
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
+      <PopularMeals />
     </Fragment>
   );
 };
---
+++ src/components/PopularMeals/PopularMeals.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+import classes from "./PopularMeals.module.css";
+import Fish from "../../images/card-img-1.png";
+import Cake from "../../images/card-img-2.png";
+import Lobster from "../../images/card-img-3.png";
+
+const PopularMeals = () => {
+  return (
+    <Fragment>
+      <section className={classes["section-2"]}>
+        <h1 className={classes["section-heading"]}>popular meals</h1>
+        <div className={classes["cards-container"]}>
+          <div className={classes.card}>
+            <img src={Fish} className={classes["card-img"]} />
+            <h3 className={classes["card-name"]}>Fish</h3>
+            <button className={classes["card-btn"]}>Order now</button>
+          </div>
+          <div className={classes.card}>
+            <img src={Cake} className={classes["card-img"]} />
+            <h3 className={classes["card-name"]}>Cake</h3>
+            <button className={classes["card-btn"]}>Order now</button>
+          </div>
+          <div className={classes.card}>
+            <img src={Lobster} className={classes["card-img"]} />
+            <h3 className={classes["card-name"]}>Lobster</h3>
+            <button className={classes["card-btn"]}>Order now</button>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default PopularMeals;
